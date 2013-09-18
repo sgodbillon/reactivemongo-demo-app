c170ab3f47720b8190952cce72d8e36f0fffb4d9
Stephane Godbillon <stephane@godbillon.fr>
2013-09-18 12:39:05 +0200

    Update to ReactiveMongo 0.10.0-SNAPSHOT

--- project/Build.scala
+++ project/Build.scala
@@ -1,16 +1,18 @@
 import sbt._
 import Keys._
-import PlayProject._
 
 object ApplicationBuild extends Build {
 
   val appName = "mongo-app"
   val appVersion = "1.0-SNAPSHOT"
 
+  scalaVersion := "2.10.2"
+
   val appDependencies = Seq(
-    "org.reactivemongo" %% "play2-reactivemongo" % "0.9")
+    "org.reactivemongo" %% "play2-reactivemongo" % "0.10.0-SNAPSHOT")
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
+    resolvers += "Sonatype snapshots" at "http://oss.sonatype.org/content/repositories/snapshots/"
       // settings
   )
 
